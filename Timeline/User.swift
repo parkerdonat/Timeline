1ea80ad30370d7f091f2e9661ba226b6a5e1d998
Parker Donat <parkerdonat@gmail.com>
2016-02-23 16:10:57 -0700

    User model

--- Timeline/User.swift
+++ Timeline/User.swift
@@ -8,9 +8,21 @@
 
 import Foundation
 
-struct User {
+struct User: Equatable {
     var username = ""
+    var bio: String?
+    var url: String?
+    var identifier: String?
     
+    init(username: String, bio: String? = nil, url: String? = nil, uid: String) {
+        self.username = username
+        self.bio = bio
+        self.url = url
+        self.identifier = uid
+    }
 }
 
+func ==(lhs: User, rhs: User) -> Bool {
+    return (lhs.username == rhs.username) && (lhs.identifier == rhs.identifier)
+}
 
