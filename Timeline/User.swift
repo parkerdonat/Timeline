38be67d54acac89bb96b73a60cfb249a4d9240b3
Parker Donat <parkerdonat@gmail.com>
2016-02-23 15:45:10 -0700

    Finished View Heirarchy

---
+++ Timeline/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Timeline
+//
+//  Created by Parker Donat on 2/23/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var username = ""
+    
+}
+
+
